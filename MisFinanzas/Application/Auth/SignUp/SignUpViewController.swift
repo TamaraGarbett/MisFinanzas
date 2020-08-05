6370f04093b16418374f2e41e1ecb8087a3fc2c7
tamaragarbett <tamaragarbett13@gmail.com>
2020-08-05 20:22:46 -0300

    Refactoring

--- MisFinanzas/Application/Auth/SignUp/SignUpViewController.swift
+++ MisFinanzas/Application/Auth/SignUp/SignUpViewController.swift
@@ -4,8 +4,6 @@ class SignUpViewController: BaseViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
     }
 
    
