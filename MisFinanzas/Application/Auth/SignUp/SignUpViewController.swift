0c0a3c2210d3dc52e5036be53e9f516b22206cc8
tamaragarbett <tamaragarbett13@gmail.com>
2020-08-05 20:58:23 -0300

    LoginViewController created

--- MisFinanzas/Application/Auth/SignUp/SignUpViewController.swift
+++ MisFinanzas/Application/Auth/SignUp/SignUpViewController.swift
@@ -1,9 +1,25 @@
 import UIKit
 
 class SignUpViewController: BaseViewController {
-
+    @IBOutlet var txtEmail:UITextField!
+    @IBOutlet var txtPassword:UITextField!
+    @IBOutlet var btnLogin:UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+    }
+    
+    @IBAction func btnLogin_Action(){
+        
+    }
+    
+    @IBAction func btnForgotPassword(){
+        
+    }
+    
+    @IBAction func btnSignUp(){
+        
     }
 
    
--- MisFinanzas/Application/Entities/User.swift
+++ MisFinanzas/Application/Entities/User.swift
@@ -1,1 +1,22 @@
 import Foundation
+class User{
+    private var email:String
+    private var password:String
+    
+    func getEmail() -> String{
+        return self.email
+    }
+    func setEmail(newEmail:String){
+        self.email = newEmail
+    }
+    func getPassword() -> String{
+        return self.password
+    }
+    func setPassword(newPassword:String){
+        self.password = newPassword
+    }
+    
+    init (email:String, password:String){
+        self.email = email
+        self.password = password
+}
